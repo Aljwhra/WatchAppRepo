eb71213484e94ae015234205ed47a928d33130f4
Aljwhra <aljwhra.n2@icloud.com>
2023-01-10 23:13:19 +0300

    Initial Commit

---
+++ WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by Aljwhra Alnasser on 17/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
